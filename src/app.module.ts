218438e587f5f5bfbf6cc388978b4722d7f80843
A. Mestra (Rain) <adonymestra2@gmail.com>
2023-10-21 03:39:02 -0500

    Tokens

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,26 +8,57 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { TokenService } from './token/token.service';
+
+const pubSub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    retryStrategy: (times) => {
+      return Math.min(times * 50, 2000);
+    }
+  }
+})
+
 
 @Module({
-  imports: 
-  [
-    AuthModule, 
-    UserModule,
-    GraphQLModule.forRootAsync({
-      imports: [ConfigModule, AppModule],
-      inject: [ConfigService],
-      driver: ApolloDriver,
-      useFactory: async (configService: ConfigService) => ({
-        playground: true,
-        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-        sortSchema: true,
-      })
-    }),
+  imports:
+    [
+      AuthModule,
+      UserModule,
+      GraphQLModule.forRootAsync({
+        imports: [ConfigModule, AppModule],
+        inject: [ConfigService],
+        driver: ApolloDriver,
+        useFactory: async (configService: ConfigService, tokenService: TokenService) => ({
+          installSubscriptionHandlers: true,
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+          subscriptions: {
+            'graphql-ws': true,
+            'subscriptions-transport-ws': true,
+          },
+          onConnect: async (connectionParams) => {
+            const token = tokenService.extractToken(connectionParams);
+            if(!token){
+              throw new Error('Token not provided!');
+            }
 
-    ConfigModule.forRoot({isGlobal: true})
-  ],
+            const user = tokenService.validateToken(token);
+            if(!user){
+              throw new Error('Invalid Token');
+            }
+
+            return { user };
+          },
+        })
+      }),
+
+      ConfigModule.forRoot({ isGlobal: true })
+    ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TokenService],
 })
-export class AppModule {}
+export class AppModule { }
